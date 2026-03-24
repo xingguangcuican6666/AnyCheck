472761bd45f05d2897a6529b63c25ec1bf81a31a
xingguangcuican <xingguangcuican666@foxmail.com>
2026-03-24 11:30:41 +0800

    Update app version to 1.0.7 in MainActivity

--- app/src/main/kotlin/com/anycheck/app/MainActivity.kt
+++ app/src/main/kotlin/com/anycheck/app/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
                 if (showAbout) {
                     AboutScreen(
-                        appVersion = "1.0.4",
+                        appVersion = "1.0.7",
                         onNavigateBack = { showAbout = false }
                     )
                 } else {
