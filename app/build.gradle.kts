6a4a608e6fe2184c5a06f1999f7bc47c6dc3ba9a
xingguangcuican <xingguangcuican666@foxmail.com>
2026-03-26 21:13:07 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.anycheck.app"
         minSdk = 26
-        targetSdk = 28
+        targetSdk = 36
         versionCode = 10100
         versionName = "1.1.0"
 
